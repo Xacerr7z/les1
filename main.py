a1c93fd8ad7de2d7bbce61817a20f7f74cc005d1
Xacerr7z <169067831+Xacerr7z@users.noreply.github.com>
2024-05-10 12:18:03 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+userInfo = {'id': 8576849, 'username': 'виталик99', 'email': 'виталик-656', "isAuth": True }
+# print(userInfo['email'])
+# print(list(userInfo.items()))
+# print(list(myDict.keys()))
+
+userInfo.update(blue=2)
+print(userInfo)
+
+
+
+
+
+
+
+
+
+
+
+733f5564
+
+
+
+
+
