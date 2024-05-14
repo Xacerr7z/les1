0495ecf22af02a6af56a29a8860b975151df2aa7
Xacerr7z <169067831+Xacerr7z@users.noreply.github.com>
2024-05-14 12:35:42 +0600

    Add files via upload

---
+++ les2/vuij.py
@@ -0,0 +1,25 @@
+# import os
+# print(os.getcwd())
+
+# import time, random
+
+# print("i go  sleep")
+# time.sleep(1)
+# print("i wake up")
+
+
+# myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, ]
+
+#print(random.choice(myArr))
+
+#import ugb
+
+#result = ugb.sum(3, 2)
+
+#print(result)
+
+#from hfrnrh import min
+#result1 = min(5, 3)
+#print(result1)
+
+
